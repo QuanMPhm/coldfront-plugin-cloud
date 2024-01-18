dd9786307121435b90590abbaa9e267333ab4fe4
Quan Pham <qmpham2019@gmail.com>
2024-01-18 10:40:40 -0500

    Fixed ci test file

--- ci/run_functional_tests_openshift.sh
+++ ci/run_functional_tests_openshift.sh
@@ -14,5 +14,10 @@ export DJANGO_SETTINGS_MODULE="local_settings"
 export FUNCTIONAL_TESTS="True"
 export OS_AUTH_URL="https://onboarding-onboarding.cluster.local"
 
+export ACCT_MGT_ADMIN_USERNAME=admin
+export ACCT_MGT_ADMIN_PASSWORD=pass
+export ACCT_MGT_IDENTITY_PROVIDER=developer
+export ACCT_MGT_OPENSHIFT_URL=https://172.17.0.2:6443
+
 coverage run --source="." -m django test coldfront_plugin_cloud.tests.functional.openshift
 coverage report
